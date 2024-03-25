9f935fef8478150ccc36adc17d47ae86cff0e419
XTer <xxoy1234@outlook.com>
2024-03-26 01:43:56 +0800

    修正英文bat

--- 0 Bat Files/0 Update.bat
+++ 0 Bat Files/0 Update.bat
@@ -12,12 +12,9 @@ cd /d %REPO_PATH%
 echo setting the PortableGit path
 set GIT_PATH=PortableGit/bin
 
-echo Update submodule
-"%GIT_PATH%\git.exe" submodule update --init --recursive
-
 echo git pull
 "%GIT_PATH%\git.exe" stash
-"%GIT_PATH%\git.exe" pull https://gitee.com/xxoy/GPT-SoVITS-Inference.git main
+"%GIT_PATH%\git.exe" pull https://github.com/X-T-E-R/GPT-SoVITS-Inference.git stable
 
 echo.
 pause
--- 0 Bat Files/999 Force Updating.bat
+++ 0 Bat Files/999 Force Updating.bat
@@ -13,12 +13,9 @@ echo setting the PortableGit path
 set GIT_PATH=PortableGit/bin
 
 
-echo Update submodule
-"%GIT_PATH%\git.exe" submodule update --init --recursive
-"%GIT_PATH%\git.exe" submodule foreach --recursive "git fetch origin plug_in && git reset --hard origin/plug_in"
 
 echo git reset --hard
-"%GIT_PATH%\git.exe" fetch https://gitee.com/xxoy/GPT-SoVITS-Inference.git main
+"%GIT_PATH%\git.exe" fetch https://github.com/X-T-E-R/GPT-SoVITS-Inference.git stable
 "%GIT_PATH%\git.exe" reset --hard FETCH_HEAD
 
 echo.
